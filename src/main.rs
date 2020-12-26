8f3496571ecb976d9ea4495c655936cfb95cb979
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-12-26 11:29:57 +0700

    Added loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -14,9 +15,26 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    // Brainfuck instructions
     let mut instructions: Vec<char> = Vec::new();
+
+    // Cycle positions
+    let mut opened: Vec<usize> = Vec::new();
+    let mut blocks: HashMap<usize, usize> = HashMap::new();
+
     for line in reader.lines() {
         for c in line.expect("Lines failed!").chars() {
+            let i = instructions.len();
+            if c == '[' {
+                opened.push(i);
+            } else if c == ']' {
+                let pos = match opened.pop() {
+                    Some(number) => number,
+                    None => panic!("Program execution error!"),
+                };
+                blocks.insert(i, pos);
+                blocks.insert(pos, i);
+            }
             instructions.push(c);
         }
     }
@@ -39,6 +57,20 @@ fn main() {
             print!("{} ", cells[pos]);
         } else if instructions[i] == ',' {
             // read
+        } else if instructions[i] == '[' {
+            if cells[pos] == 0 {
+                i = match blocks.get(&i) {
+                    Some(number) => *number,
+                    None => panic!("Program execution error"),
+                };
+            }
+        } else if instructions[i] == ']' {
+            if cells[pos] != 0 {
+                i = match blocks.get(&i) {
+                    Some(number) => *number,
+                    None => panic!("Program execution error"),
+                };
+            }
         }
         i += 1;
     }
