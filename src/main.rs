d92b06fd15c1918164bec8abeffc508dd8bd5409
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2024-01-14 08:54:46 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,6 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let mut rl = Editor::<()>::new();
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("Unknown file name!");
-    }
-
-    let filename = &args[1];
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-
     // Brainfuck instructions
     let mut instructions: Vec<char> = Vec::new();
 
@@ -23,25 +14,69 @@ fn main() {
     let mut opened: Vec<usize> = Vec::new();
     let mut blocks: HashMap<usize, usize> = HashMap::new();
 
-    for line in reader.lines() {
-        for c in line.expect("Parsing: lines failed!").chars() {
-            let i = instructions.len();
-            if c == '[' {
-                opened.push(i);
-            } else if c == ']' {
-                let pos = match opened.pop() {
-                    Some(number) => number,
-                    None => panic!("Parsing: program execution error!"),
-                };
-                blocks.insert(i, pos);
-                blocks.insert(pos, i);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        loop {
+            match rl.readline("> ") {
+                Ok(line) => {
+                    for c in line.chars() {
+                        parse(c, &mut instructions, &mut opened, &mut blocks);
+                    }
+
+                    eval(&instructions, &blocks);
+
+                    instructions = Vec::new();
+                    opened = Vec::new();
+                    blocks = HashMap::new();
+                }
+                Err(_) => panic!("Read: keyboard read error!"),
             }
-            instructions.push(c);
         }
+    } else {
+        let filename = &args[1];
+        let file = File::open(filename).unwrap();
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            for c in line.expect("Parsing: lines failed!").chars() {
+                parse(c, &mut instructions, &mut opened, &mut blocks);
+            }
+        }
+
+        eval(&instructions, &blocks);
     }
+}
+
+fn parse(
+    c: char,
+    instructions: &mut Vec<char>,
+    opened: &mut Vec<usize>,
+    blocks: &mut HashMap<usize, usize>,
+) {
+    let i = instructions.len();
+
+    if c == '[' {
+        opened.push(i);
+    } else if c == ']' {
+        let pos = match opened.pop() {
+            Some(number) => number,
+            None => panic!("Parsing: program execution error!"),
+        };
+
+        blocks.insert(i, pos);
+        blocks.insert(pos, i);
+    }
+
+    instructions.push(c);
+}
+
+fn eval(instructions: &Vec<char>, blocks: &HashMap<usize, usize>) {
+    let mut rl = Editor::<()>::new();
 
     let mut cells: Vec<u64> = vec![0; 30000];
     let mut pos = 0;
+
+    let mut output: String = String::new();
 
     let mut i = 0;
     while i < instructions.len() {
@@ -54,9 +89,10 @@ fn main() {
         } else if instructions[i] == '-' {
             cells[pos] -= 1;
         } else if instructions[i] == '.' {
-            print!("{} ", cells[pos]);
+            output.push_str(format!("{} ", cells[pos]).as_str());
         } else if instructions[i] == ',' {
             let readline = rl.readline(">> ");
+
             match readline {
                 Ok(line) => {
                     cells[pos] = line.parse::<u64>().expect("Read: value parsing error!");
@@ -80,4 +116,6 @@ fn main() {
         }
         i += 1;
     }
+
+    println!("{}ok", output);
 }
